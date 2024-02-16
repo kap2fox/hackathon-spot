5be0b299025d6bbd585185ebdedfebf0c3f7e528
kap2fox <kap2fox@gmail.com>
2024-02-16 04:31:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 import time
 from spot_controller import SpotController
 
-ROBOT_IP = "192.168.50.3"#os.environ['ROBOT_IP']
+ROBOT_IP = "10.0.0.3"#os.environ['ROBOT_IP']
 SPOT_USERNAME = "admin"#os.environ['SPOT_USERNAME']
 SPOT_PASSWORD = "2zqa8dgw7lor"#os.environ['SPOT_PASSWORD']
 
 
 def main():
     #example of using micro and speackers
-    print("Start recording audio")
+    """print("Start recording audio")
     sample_name = "aaaa.wav"
     cmd = f'arecord -vv --format=cd --device={os.environ["AUDIO_INPUT_DEVICE"]} -r 48000 --duration=30 -c 1 {sample_name}'
     print(cmd)
@@ -22,7 +22,7 @@ def main():
     camera_capture = cv2.VideoCapture(0)
     rv, image = camera_capture.read()
     print(f"Image Dimensions: {image.shape}")
-    camera_capture.release()
+    camera_capture.release()"""
   
     # Use wrapper in context manager to lease control, turn on E-Stop, power on robot and stand up at start
     # and to return lease + sit down at the end
@@ -37,13 +37,13 @@ def main():
                                  sleep_after_point_reached=1)
         time.sleep(3)
 
-        # Make Spot to move by goal_x meters forward and goal_y meters left
+        """# Make Spot to move by goal_x meters forward and goal_y meters left
         spot.move_to_goal(goal_x=0.5, goal_y=0)
         time.sleep(3)
 
         # Control Spot by velocity in m/s (or in rad/s for rotation)
         spot.move_by_velocity_control(v_x=-0.3, v_y=0, v_rot=0, cmd_duration=2)
-        time.sleep(3)
+        time.sleep(3)"""
 
 
 if __name__ == '__main__':
